9c6fddaa3f3fafc8ff5a73f78f5603a55a954bc6
Matheus Magnago <magnagomatheus7@gmail.com>
2025-09-13 09:37:37 -0300

    adding comments and implementing file reader class.

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,34 @@
 public class Main {
     public static void main(String[] args) {
 
+    // Creating both types of ordered lists, by name and by registry.
     LinkedListWithComparator<Student> listOrderedName = new LinkedListWithComparator<Student>(true, new StudentComparatorByName());
     LinkedListWithComparator<Student> listOrderedRegistry = new LinkedListWithComparator<Student>(true, new StudentComparatorByRegistry());
 
+    // Creating students
     Student s1 = new Student(1, "Ze", 12);
     Student s2 = new Student(2, "Mane", 12345);
     Student s3 = new Student(3, "Josep", 777);
 
+    // Inserting students at the list ordered by name
     listOrderedName.insert(s1);
     listOrderedName.insert(s2);
     listOrderedName.insert(s3);
 
+        // Inserting students at the list ordered by registry
     listOrderedRegistry.insert(s1);
     listOrderedRegistry.insert(s2);
 
+    // Verifying if a student exists using the registry at the list ordered by registry
     listOrderedRegistry.elemExists(s1);
     listOrderedRegistry.elemExists(s2);
 
+    // Searching a student at the list ordered by registry
     listOrderedRegistry.search(new Student(3, null, 0));
+    // Searching a student at the list ordered by name
     listOrderedName.search(new Student(3, "Ze", 0));
 
+    // Printing both lists
     System.out.println(listOrderedName);
     System.out.println(listOrderedRegistry);
     }
