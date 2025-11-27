3c2b9812e57fb8f81feb47dcfebf9bdafbfd853f
Matheus Magnago <magnagomatheus7@gmail.com>
2025-11-27 17:52:34 -0300

    testing lgpd user input

--- src/Main.java
+++ src/Main.java
@@ -44,6 +44,8 @@ public class Main {
         System.out.println("groq test");
 
         System.out.println("LGPDDDDDDDDDDDDDDDDDDDDDDDDD");
+        System.out.println("LGPDDDDDDDDDDDDDDDDDDDDDDDDD");
+        System.out.println("LGPDDDDDDDDDDDDDDDDDDDDDDDDD");
         System.out.println("laflafsp[afl[pfasl");
 
 
